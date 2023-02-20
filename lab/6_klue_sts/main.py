1e80a2de4b1619d8078fc7d796d0de284c950bf3
hi-jin <crushed7@o.cnu.ac.kr>
2023-02-20 13:41:40 +0900

    학습 방식 오류 수정

--- lab/6_klue_sts/main.py
+++ lab/6_klue_sts/main.py
@@ -1,275 +1,240 @@
 import argparse
 
-import numpy as np
-
 import torch
-from torch.utils.data import Dataset, DataLoader
-
 import torchmetrics
-
 import pytorch_lightning as pl
-from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 
 from datasets import load_dataset
-from transformers import AutoTokenizer, GPT2LMHeadModel
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
-class Sentence2SentenceDataset():
-    def __init__(self,
-                 input_sentences,
-                 output_sentences,
-                 tokenizer,
-                 max_seq_length,
-                 **kwargs) -> None:
-        self.input_sentences = input_sentences
-        self.output_sentences = output_sentences
+class SentenceDataset(torch.utils.data.Dataset):
+    def __init__(
+                    self,
+                    sentence1,
+                    sentence2,
+                    tokenizer,
+                ):
+        super().__init__()
+        self.sentence1 = sentence1
+        self.sentence2 = sentence2
         self.tokenizer = tokenizer
-        self.max_seq_length = max_seq_length
-
+    
     
     def __len__(self):
-        return len(self.input_sentences)
+        return len(self.sentence1)
+
     
-    
-    def __getitem__(self, index):
-        input_sentence = self.input_sentences[index]
-        output_sentence = self.output_sentences[index]
-        
-        inputs = self.tokenizer(
-            input_sentence,
-            padding="max_length",
-            truncation=True,
-            max_length=self.max_seq_length,
-            return_tensors="pt",
-        )
-        outputs = self.tokenizer(
-            output_sentence,
-            padding="max_length",
-            truncation=True,
-            max_length=self.max_seq_length,
-            return_tensors="pt",
-        )
-        
+    def __getitem__(self, idx):
+        inputs = self.tokenizer(self.sentence1[idx] + " " + self.sentence2[idx], return_tensors="pt")
         input_ids = inputs["input_ids"]
         attention_mask = inputs["attention_mask"]
-        output_ids = outputs["input_ids"]
-
-        return (
-            ### inputs
-            input_ids.squeeze(0),
-            attention_mask.squeeze(0),
-            
-            ### labels
-            output_ids.squeeze(0)
-        )
+        
+        return (input_ids.squeeze(0), attention_mask.squeeze(0))
 
 
 class KLUEDatamodule(pl.LightningDataModule):
-    def __init__(self, 
-                 dataset_name, 
-                 dataset_subset_name,
-                 pretrained_model_name,
-                 max_seq_length,
-                 batch_size,
-                 num_workers,
-                 tokenizer,
-                 **kwargs) -> None:
+    def __init__(
+                    self,
+                    dataset_path,
+                    dataset_name,
+                    tokenizer,
+                    batch_size,
+                    num_workers,
+                ):
         super().__init__()
+        self.dataset_path = dataset_path
         self.dataset_name = dataset_name
-        self.dataset_subset_name = dataset_subset_name
-        self.pretrained_model_name = pretrained_model_name
-        self.max_seq_length = max_seq_length
+        self.tokenizer = tokenizer
         self.batch_size = batch_size
         self.num_workers = num_workers
-        self.tokenizer = tokenizer
     
     
-    def prepare_data(self) -> None:
-        load_dataset(self.dataset_name, self.dataset_subset_name)
-
+    def prepare_data(self):
+        load_dataset(self.dataset_path, self.dataset_name)
     
-    def setup(self, stage: str) -> None:
-        all_data = load_dataset(self.dataset_name, self.dataset_subset_name)
-        train_test_data = all_data["train"].train_test_split(train_size=0.8, test_size=0.2, shuffle=True)
+    
+    def setup(self, stage=None):
+        all_data = load_dataset(self.dataset_path, self.dataset_name)
+        train_test_data = all_data["train"].train_test_split(test_size=0.2)
+        train_data = train_test_data["train"]
+        test_data = train_test_data["test"]
+        val_data = all_data["validation"]
 
-        self.train_data = train_test_data["train"]
-        self.test_data = train_test_data["test"]
-        self.val_data = all_data["validation"]
-
-        if stage == "fit":
-            self.train_dataset = Sentence2SentenceDataset(
-                self.train_data["sentence1"],
-                self.train_data["sentence2"],
-                self.tokenizer,
-                self.max_seq_length,
-            )
-            self.val_dataset = Sentence2SentenceDataset(
-                self.val_data["sentence1"],
-                self.val_data["sentence2"],
-                self.tokenizer,
-                self.max_seq_length,
-            )
-        if stage == "test":
-            self.test_dataset = Sentence2SentenceDataset(
-                self.test_data["sentence1"],
-                self.test_data["sentence2"],
-                self.tokenizer,
-                self.max_seq_length,
-            )
+        self.train_dataset = SentenceDataset(
+                                                train_data["sentence1"],
+                                                train_data["sentence2"],
+                                                self.tokenizer,
+                                            )
+        self.test_dataset = SentenceDataset(
+                                                test_data["sentence1"],
+                                                test_data["sentence2"],
+                                                self.tokenizer,
+                                            )
+        self.val_dataset = SentenceDataset(
+                                                val_data["sentence1"],
+                                                val_data["sentence2"],
+                                                self.tokenizer,
+                                           )
     
     
     def train_dataloader(self):
-        return DataLoader(
-            self.train_dataset,
-            batch_size=self.batch_size,
-            num_workers=self.num_workers,
-            shuffle=True,
-        )
-
-
-    def val_dataloader(self):
-        return DataLoader(
-            self.val_dataset,
-            batch_size=self.batch_size,
-            num_workers=self.num_workers,
-        )
+        return torch.utils.data.DataLoader(
+                                                self.train_dataset,
+                                                batch_size=self.batch_size,
+                                                num_workers=self.num_workers,
+                                                shuffle=True,
+                                                collate_fn=self.collate_fn,
+                                            )
     
-
+    
     def test_dataloader(self):
-        return DataLoader(
-            self.test_dataset,
-            batch_size=self.batch_size,
-            num_workers=self.num_workers,
-        )
+        return torch.utils.data.DataLoader(
+                                                self.test_dataset,
+                                                batch_size=self.batch_size,
+                                                num_workers=self.num_workers,
+                                                shuffle=False,
+                                                collate_fn=self.collate_fn,
+                                            )
+    
+    
+    def val_dataloader(self):
+        return torch.utils.data.DataLoader(
+                                                self.val_dataset,
+                                                batch_size=self.batch_size,
+                                                num_workers=self.num_workers,
+                                                shuffle=False,
+                                                collate_fn=self.collate_fn,
+                                            )
+    
+    
+    def collate_fn(self, batch):
+        input_ids, attention_mask = zip(*batch)
+        input_ids = torch.nn.utils.rnn.pad_sequence(input_ids, batch_first=True, padding_value=self.tokenizer.pad_token_id)
+        attention_mask = torch.nn.utils.rnn.pad_sequence(attention_mask, batch_first=True, padding_value=0)
+        return (input_ids, attention_mask)
 
 
-class TextGenerationModel(pl.LightningModule):
-    def __init__(self,
-                 pretrained_model_name,
-                 tokenizer,
-                 lr,
-                 **kwargs) -> None:
+class STSModel(pl.LightningModule):
+    def __init__(
+                    self,
+                    model_name,
+                    tokenizer,
+                    lr,
+                ):
         super().__init__()
         self.save_hyperparameters(ignore=["tokenizer"])
-        
-        self.model = GPT2LMHeadModel.from_pretrained(pretrained_model_name)
+        self.model = AutoModelForCausalLM.from_pretrained(model_name)
         self.tokenizer = tokenizer
         self.lr = lr
     
     
-    def forward(self, input_ids, attention_mask, labels=None):
-        if labels is not None:
-            labels = labels.masked_fill(attention_mask == 0, -100)
-
-        outputs = self.model(
-            input_ids=input_ids,
-            attention_mask=attention_mask,
-            labels=labels
-        )
-        
-        return outputs.loss, outputs.logits
+    def forward(self, input_ids, attention_mask):
+        outputs = self.model(input_ids=input_ids, attention_mask=attention_mask)
+        logits = outputs.logits
+        return logits
     
     
     def training_step(self, batch, batch_idx):
-        input_ids, attention_mask, labels = batch
-        loss, logits = self(input_ids, attention_mask, labels)
+        input_ids, attention_mask = batch
+        logits = self(input_ids=input_ids, attention_mask=attention_mask)  # [batch_size, seq_len, vocab_size]
+        loss = torch.nn.functional.cross_entropy(torch.permute(logits, (0, 2, 1)), input_ids)
         self.log("train_loss", loss)
         return loss
     
     
     def validation_step(self, batch, batch_idx):
-        input_ids, attention_mask, labels = batch
-        loss, logits = self(input_ids, attention_mask, labels)
+        input_ids, attention_mask = batch
+        logits = self(input_ids=input_ids, attention_mask=attention_mask)
+        loss = torch.nn.functional.cross_entropy(torch.permute(logits, (0, 2, 1)), input_ids)
+        preds = self.tokenizer.batch_decode(torch.argmax(logits, dim=-1), skip_special_tokens=True)
+        inputs = self.tokenizer.batch_decode(input_ids, skip_special_tokens=True)
+
+        preds = list(map(lambda sentence: " ".join(self.tokenizer.tokenize(sentence)), preds))
+        inputs = list(map(lambda sentence: " ".join(self.tokenizer.tokenize(sentence)), inputs))
         
-        scores = []
-        preds = self.tokenizer.batch_decode(logits.argmax(dim=-1))
-        targets = self.tokenizer.batch_decode(labels.masked_fill(attention_mask == 0, self.tokenizer.pad_token_id))
-        for pred, target in zip(preds, targets):
-            score = torchmetrics.functional.bleu_score([pred], [[target]])
-            scores.append(score)
-        
-        bleu_score = np.mean(scores)
-        
-        metrics = {
+        bleu_score = torchmetrics.functional.bleu_score(preds, inputs)
+        self.log_dict({
             "val_loss": loss,
             "val_bleu_score": bleu_score,
-        }
-        
-        self.log_dict(metrics)
-        return metrics
+        })
     
     
     def test_step(self, batch, batch_idx):
-        input_ids, attention_mask, labels = batch
-        loss, logits = self(input_ids, attention_mask, labels)
+        input_ids, attention_mask = batch
+        logits = self(input_ids=input_ids, attention_mask=attention_mask)
+        loss = torch.nn.functional.cross_entropy(torch.permute(logits, (0, 2, 1)), input_ids)
+        preds = self.tokenizer.batch_decode(torch.argmax(logits, dim=-1), skip_special_tokens=True)
+        inputs = self.tokenizer.batch_decode(input_ids, skip_special_tokens=True)
+
+        preds = list(map(lambda sentence: " ".join(self.tokenizer.tokenize(sentence)), preds))
+        inputs = list(map(lambda sentence: " ".join(self.tokenizer.tokenize(sentence)), inputs))
         
-        scores = []
-        preds = self.tokenizer.batch_decode(logits.argmax(dim=-1))
-        targets = self.tokenizer.batch_decode(labels.masked_fill(attention_mask == 0, self.tokenizer.pad_token_id))
-        for pred, target in zip(preds, targets):
-            score = torchmetrics.functional.bleu_score([pred], [[target]])
-            scores.append(score)
-        
-        bleu_score = np.mean(scores)
-        
-        metrics = {
+        bleu_score = torchmetrics.functional.bleu_score(preds, inputs)
+        self.log_dict({
             "test_loss": loss,
             "test_bleu_score": bleu_score,
-        }
-        
-        self.log_dict(metrics)
-        return metrics
-        
-        
+        })
+    
+    
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), self.lr)
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
 
 
-def parse_args():
+def load_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset_name", type=str, default="klue")
-    parser.add_argument("--dataset_subset_name", type=str, default="sts")
-    parser.add_argument("--pretrained_model_name", type=str, default="skt/kogpt2-base-v2")
-    parser.add_argument("--max_seq_length", type=int, default=64)
+    parser.add_argument("--seed", type=int, default=1234)
+    parser.add_argument("--dataset_path", type=str, default="klue")
+    parser.add_argument("--dataset_name", type=str, default="sts")
+    parser.add_argument("--model_name", type=str, default="skt/kogpt2-base-v2")
     parser.add_argument("--batch_size", type=int, default=64)
-    parser.add_argument("--num_workers", type=int, default=1)
+    parser.add_argument("--num_workers", type=int, default=12)
     parser.add_argument("--lr", type=float, default=0.0001)
-    return vars(parser.parse_args())
+    parser.add_argument("--accelerator", type=str, default="gpu")
+    parser.add_argument("--devices", type=int, default=4)
+    parser.add_argument("--strategy", type=str, default="dp")
+    args = vars(parser.parse_args())
+    return args
 
 
 def main():
-    pl.seed_everything(1234)
+    args = load_args()
     
-    args = parse_args()
-
     tokenizer = AutoTokenizer.from_pretrained(
-        args["pretrained_model_name"],
-        bos_token='</s>',
-        eos_token='</s>',
-        unk_token='<unk>',
-        pad_token='<pad>',
-        mask_token='<mask>'
-    )
+                                                args["model_name"],
+                                                bos_token='</s>', 
+                                                eos_token='</s>',
+                                                unk_token='<unk>',
+                                                pad_token='<pad>',
+                                                mask_token='<mask>',
+                                            )
+
+    datamodule = KLUEDatamodule(
+                                    args["dataset_path"],
+                                    args["dataset_name"],
+                                    tokenizer,
+                                    args["batch_size"],
+                                    args["num_workers"],
+                                )
+    datamodule.prepare_data()
     
     trainer = pl.Trainer(
-        callbacks=[
-            EarlyStopping(monitor="val_loss", mode="min", verbose=True, patience=3),
-            ModelCheckpoint("./checkpoints", "val_loss:{val_loss}-val_bleu:{val_bleu_score}", monitor="val_loss", mode="min", verbose=True)
-        ],
-        # accelerator="gpu",
-        # devices=4,
-        # strategy="dp"
-    )
+                            callbacks=[
+                                EarlyStopping(monitor="val_loss", patience=5, mode="min"),
+                                ModelCheckpoint(monitor="val_loss", mode="min", save_top_k=1)
+                            ],
+                            accelerator=args["accelerator"],
+                            devices=args["devices"],
+                            strategy=args["strategy"],
+                        )
     
-    data = KLUEDatamodule(tokenizer=tokenizer, **args)
-
-    model = TextGenerationModel(tokenizer=tokenizer, **args)
+    model = STSModel(args["model_name"], tokenizer, args["lr"])
     
-    trainer.test(model, data)
-    trainer.fit(model, data)
-    trainer.test(model, data)
+    trainer.test(model, datamodule)
+    trainer.fit(model, datamodule)
+    trainer.test(model, datamodule)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-    
